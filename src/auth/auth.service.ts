91f5a63bd5e3e614b6dd6770ed6b9adbefb88390
Hashim <hashim.a@deepnetsoft.com>
2023-11-20 12:01:19 +0530

    First Commit

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,46 @@
+import { ForbiddenException, Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
+import { AuthDto } from "./dto";
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+
+@Injectable()
+export class AuthService {
+    constructor(private prisma: PrismaService){
+
+    }
+    async signup(dto: AuthDto){
+        // Generate the passwordhash
+        const hash = await argon.hash(dto.password) 
+
+        // Save the new user in the db
+        try
+        {
+            
+            const user = await this.prisma.user.create({
+                data: {
+                    email: dto.email,
+                    hash
+                },
+            })
+
+            delete user.hash
+
+            // return the saved user
+            return user
+
+        }
+        catch(error)
+        {
+            if(error instanceof PrismaClientKnownRequestError){
+                if(error.code == 'P2002'){
+                    throw new ForbiddenException('Credentials already taken')
+                }
+            }
+            throw error;
+        }
+    }
+    signin(){
+        return {msg : "I have Signed In"};
+    }
+}
