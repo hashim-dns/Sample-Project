e5cc94e2047705a7906784e933e47530f2a5c2c8
Hashim <hashim.a@deepnetsoft.com>
2023-11-23 21:50:16 +0530

    update

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,10 +3,17 @@ import { PrismaService } from "src/prisma/prisma.service";
 import { AuthDto } from "./dto";
 import * as argon from 'argon2';
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+import { JwtService } from "@nestjs/jwt";
+import { ConfigService } from "@nestjs/config";
+import { promises } from "dns";
 
 @Injectable()
 export class AuthService {
-    constructor(private prisma: PrismaService){
+    constructor(
+        private prisma: PrismaService,
+        private jwt: JwtService,
+        private config: ConfigService
+    ) {
 
     }
     async signup(dto: AuthDto){
@@ -27,20 +34,54 @@ export class AuthService {
             delete user.hash
 
             // return the saved user
-            return user
+            return this.signToken(user.id, user.email)
 
         }
         catch(error)
         {
             if(error instanceof PrismaClientKnownRequestError){
                 if(error.code == 'P2002'){
-                    throw new ForbiddenException('Credentials already taken')
+                    throw new ForbiddenException('Credentials already taken,You cannot use that')
                 }
             }
             throw error;
         }
     }
-    signin(){
-        return {msg : "I have Signed In"};
+
+    async signin(dto: AuthDto){
+        // Find the user by Email
+        const user = await this.prisma.user.findUnique({
+            where:  {
+                email: dto.email,
+            }
+        })
+        // If user doesn't exist throw Exception
+        if(!user){
+            throw new ForbiddenException('Credentials are incorrect')
+        }
+        // Compare Password
+        const passMatches = await argon.verify(user.hash, dto.password)
+        // If password is incorrect throw
+        if(!passMatches){
+            throw new ForbiddenException('Credentials are incorrect')
+        }
+        // If correct Sign in
+        return this.signToken(user.id, user.email)
+        // return {msg : "I have Signed In"};
+    }
+
+    async signToken(userId: number, email: string):Promise<{access_token: string}>{
+        const payload = {
+            sub: userId,
+            email
+        }
+        const secret = this.config.get('JWT_SECRET')
+        const token = await this.jwt.signAsync(payload,{
+            expiresIn: '15m',
+            secret: secret
+        }) 
+        return {
+            access_token: token,
+        }   
     }
 }
